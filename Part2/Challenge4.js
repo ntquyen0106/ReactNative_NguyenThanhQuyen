ef39233263e82e11a5b74e777924c23620c3f7a4
ntquyen0106 <thanhquyen1514700@gmail.com>
2024-08-30 08:39:17 +0700

    update Part2

---
+++ Part2/Challenge4.js
@@ -0,0 +1,27 @@
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(bills[i] + tip);
+}
+
+console.log("Bills:", bills);
+console.log("Tips:", tips);
+console.log("Totals:", totals);
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+};
+
+console.log(`Average total: ${calcAverage(totals).toFixed(2)}`);
